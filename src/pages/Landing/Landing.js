2439717505243f76f97354351719eb096464b2b5
Phillip K <phillkin@gmail.com>
2020-06-28 19:28:24 -0400

    Setup react scaffolding. Adds a fullscreen green landing page

---
+++ src/pages/Landing/Landing.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import './Landing.scss';
+
+function Landing() {
+
+    return (
+        <Container className='fill-window Landing'>
+            <Col>
+                <span>
+                    greentea
+                </span>
+            </Col>
+        </Container>
+    )
+}
+
+export default Landing;
